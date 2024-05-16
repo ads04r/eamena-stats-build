4feb8b458d6c73e0c94dae3c68f5e13f699bcfb6
Ash <ash.smith@soton.ac.uk>
2024-05-16 14:54:44 +0100

    dev version of build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,4 @@ npm run build
 touch dist/.nojekyll
 mv old-dist/.git dist/
 rm -Rf old-dist
-cd dist
 chmod -R 755 *
-git add .
-git commit -a -m "Automatic commit/deploy from stoneheart"
-git push -u origin master
-
