1b9cb56545b49835018fd46e41b946dc3198ea75
Ash <ash.smith@soton.ac.uk>
2026-02-02 16:20:09 +0000

    Added empty grids to map

--- make_map.py
+++ make_map.py
@@ -1,5 +1,5 @@
 from staticmap import StaticMap, Polygon, CircleMarker
-import os, json, sys
+import os, json, sys, csv
 
 base_path = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 dist_path = os.path.join(base_path, 'dist')
@@ -9,11 +9,22 @@ static_path = os.path.join(data_path, 'static')
 grids_file = os.path.join(data_path, 'grid_data.json')
 geometry_file = os.path.join(data_path, 'grid_geometries.json')
 map_file = os.path.join(docs_path, 'map.png')
+empty_file = os.path.join(data_path, 'eamena_empty_records.csv')
+empty = []
 
 with open(grids_file, 'r') as fp:
 	data = json.load(fp)
 with open(geometry_file, 'r') as fp:
 	geometries = json.load(fp)
+with open(empty_file, newline='') as fp:
+	reader = csv.DictReader(fp)
+	for row in reader:
+		if not 'Grid Square' in row:
+			continue
+		grid_id = row['Grid Square']
+		if grid_id in empty:
+			continue
+		empty.append(grid_id)
 
 m = StaticMap(2000, 1000, url_template="https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png")
 
@@ -22,6 +33,8 @@ for grid_key in data.keys():
 	grid_id = str(grid_key)
 	item = data[grid_id]
 	fill_colour = '#CFCF7F'
+	if grid_id in empty:
+		fill_colour = '#FF2020'
 	if 'MarEA' in json.dumps(item):
 		fill_colour = '#007F00'
 	if not grid_id in geometries:
